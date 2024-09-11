ba179cc3d0e7811ae6b67142606794db55778fa0
Bismaya2401 <163625634+Bismaya2401@users.noreply.github.com>
2024-09-11 16:28:00 +0530

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -1,7 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
 # Set up the WebDriver (Chrome in this example)
@@ -11,15 +12,16 @@ try:
     # Open Google
     driver.get("https://www.google.com")
 
-    # Find the search box
-    search_box = driver.find_element("name", "q")
+    # Use WebDriverWait to wait for the search box to be present
+    wait = WebDriverWait(driver, 10)  # Wait up to 10 seconds
+    search_box = wait.until(EC.presence_of_element_located((By.NAME, "q")))
 
     # Enter "Amazon" and submit the search
     search_box.send_keys("Amazon")
     search_box.send_keys(Keys.RETURN)
 
-    # Wait for a few seconds to see the results
-    time.sleep(5)
+    # Wait for search results to be visible
+    wait.until(EC.presence_of_element_located((By.ID, "search")))
 
 finally:
     # Close the browser
