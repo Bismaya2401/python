6c8c3ab5c778a7f3a045f1554b90f141aa5a3654
Bismaya2401 <163625634+Bismaya2401@users.noreply.github.com>
2024-09-11 16:23:51 +0530

    Update google_search.py

--- google_search.py
+++ google_search.py
@@ -1,4 +1,6 @@
-from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
